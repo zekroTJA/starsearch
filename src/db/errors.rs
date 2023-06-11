e22938c167ff50313cc1efe8887a5c6c589f4b99
zekroTJA <riho@live.de>
2023-06-11 11:53:25 +0200

    rename database errors

--- src/db/errors.rs
+++ src/db/errors.rs
@@ -4,6 +4,6 @@ pub type Result<T, E = DatabaseError> = core::result::Result<T, E>;
 
 #[derive(Error, Debug)]
 pub enum DatabaseError {
-    #[error("failed inserting elements")]
-    InsertError(#[from] meilisearch_sdk::errors::Error),
+    #[error("meilisearch error")]
+    MeiliError(#[from] meilisearch_sdk::errors::Error),
 }
