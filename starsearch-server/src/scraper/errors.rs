7f364ff7b6446b974dd564ca87b258ebc963003e
Ringo Hoffmann <r.hoffmann@b12-gruppe.de>
2023-06-12 15:21:09 +0200

    add scraper error info

--- starsearch-server/src/scraper/errors.rs
+++ starsearch-server/src/scraper/errors.rs
@@ -4,6 +4,6 @@ pub type Result<T, E = ScraperError> = core::result::Result<T, E>;
 
 #[derive(Error, Debug)]
 pub enum ScraperError {
-    #[error("request failed")]
+    #[error("request failed: {0}")]
     RequestError(#[from] reqwest::Error),
 }
